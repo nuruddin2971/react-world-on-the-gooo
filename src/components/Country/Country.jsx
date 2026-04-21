6c001b64fac1e6557606851a09788a99c7f25e76
nuruddin2971 <nuruddin2971@gmail.com>
2026-04-22 01:29:00 +0800

    added all component without language

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
 
-const Country = () => {
-    return (
-        <div>
-            <h3>Name: </h3>
-        </div>
-    );
+const Country = ({ country }) => {
+  return (
+    <div className="card">
+      <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+      <h3>Name: {country.name.common}</h3>
+      <h3>Name: {country.name.official}</h3>
+      <p>ccn3: {country.ccn3.ccn3}</p>
+      <p>
+        Currencies:
+        {Object.values(country.currencies.currencies || {}).map((c) => c.name)}
+      </p>
+      <p>
+        Symbol:
+        {Object.values(country.currencies.currencies || {}).map(
+          (c) => c.symbol,
+        )}
+      </p>
+      <p>Capital: {country.capital.capital}</p>
+      <p>Region: {country.region.region}</p>
+      {/* <p>
+        Languages:
+        {Object.values(country.languages.languages || {}).map((ln) => ln.languages)}
+      </p> */}
+      <p>Area: {country.area.area}</p>
+      <p>cca3: {country.cca3.cca3}</p>
+      <p>Population: {country.population.population}</p>
+      <p>Continents: {country.continents.continents}</p>
+      {/* <p>Currencies: {country.currencies.currencies}</p> */}
+    </div>
+  );
 };
 
 export default Country;
